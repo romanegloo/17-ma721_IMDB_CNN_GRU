104b0779488816c7c82af3382382e6961942789a
Jiho Noh <bornoriginal1@gmail.com>
2017-11-30 12:10:29 -0500

    repo created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ImdbSA',
+      version='0.1',
+      description='IMDB Sentimental Analysis using CNN/LSTM',
+      author='Jiho Noh',
+      author_email='jiho.noh@uky.edu',
+      license='MIT',
+      packages=['IMDB'],
+      zip_safe=False)
